df08c868dbbd6ed9d77d33bdb3183aa07e7be92e
cheng9393 <zhujiancheng@outlook.com>
2015-12-08 23:50:33 -0500

    add a link for followers

--- routes/api.rb
+++ routes/api.rb
@@ -30,6 +30,15 @@ module Sinatra
                 content_type :json
                 Tweet.getTimeline(user_id: id).to_json
             end
+
+            #return the followers of a user of given id
+            app.get '/api/v1/users/:id/followers' do
+                id = params['id'].to_i
+                sql = "SELECT *, relations.follower_id AS id FROM users INNER JOIN relations ON users.id=relations.follower_id where relations.followee_id=#{id}"
+                result = User.connection.execute(sql)
+                content_type :json
+                return result.to_json
+            end
         end
 
       end
